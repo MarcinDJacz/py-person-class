8ee68de8737e17edc09fe7808ebc4ff39eceed4c
Marcin Djaczuk <marcindjaczuk@gmail.com>
2025-01-13 16:25:12 +0100

    Amendments

--- app/main.py
+++ app/main.py
@@ -4,28 +4,16 @@ class Person:
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-        self.wife = None
-        self.husband = None
         self.people[name] = self
 
 
 def create_person_list(people: list) -> list:
     new_list = [Person(p["name"], p["age"]) for p in people]
-    for person_id in range(len(new_list)):
-        his_wife = people[person_id].get("wife")
-        if his_wife is not None:
-            for wife_id in new_list:
-                if wife_id.name == his_wife:
-                    new_list[person_id].wife = wife_id
-        else:
-            del (new_list[person_id].wife)
-
-    for person_id in range(len(new_list)):
-        her_husband = people[person_id].get("husband")
-        if her_husband is not None:
-            for husband_id in new_list:
-                if husband_id.name == her_husband:
-                    new_list[person_id].husband = husband_id
-        else:
-            del (new_list[person_id].husband)
+    for person in people:
+        print(person)
+        persone_instance = Person.people[person["name"]]
+        if "wife" in person and person["wife"]:
+            persone_instance.wife = Person.people[person["wife"]]
+        elif "husband" in person and person["husband"]:
+            persone_instance.husband = Person.people[person["husband"]]
     return new_list
